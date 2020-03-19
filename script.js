a3801765bcabe3a3850e7ef054b07db2b74dab35
LexCapestany <56973130+LexingtonLuther@users.noreply.github.com>
2020-03-19 10:55:29 -0700

    add starter code

---
+++ script.js
@@ -0,0 +1,35 @@
+console.clear();
+const sad = document.getElementById("sad");
+const happy = document.getElementById("happy");
+setInterval(upTime, 1000);
+
+function upTime() {
+  let theTime = Number($("#theTime").text());
+  theTime = theTime + 1;
+  $("#theTime").text(theTime);
+}
+
+$("input").change(onChange);
+
+function onChange(evt) {
+  let correct = $(this).data("correct");
+  let response = $(this).val();
+  
+  console.log(md5(response));
+  if (correct == response) {
+    happy.play();
+    $(this)
+      .removeClass("incorrect")
+      .addClass("correct");
+    let theScore = Number($("#score").text());
+    theScore = theScore + 1;
+    $("#score").text(theScore);
+  } else {
+    if (Math.random() > 0.1) {
+      sad.play();
+    }
+    $(this)
+      .removeClass("correct")
+      .addClass("incorrect");
+  }
+}
